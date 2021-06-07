4ac1831511f6596dfc48a8a1a5baa6dc3403c775
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-07 11:02:20 -0700

    restructed file tree
    co-authored-by: Jesse White <jesse.white6@gmail.com>

---
+++ Travel/Migrations/20210607180107_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DestinationName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Country = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    StateProvince = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    City = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Category = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    DestinationDescription = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.DestinationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destinations");
+        }
+    }
+}
