7feaca54cc6b89b44bc220682f087149b4b0555a
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-08 11:09:21 -0700

    :beers: added swagger documentation
    co-authored-by: Jesse White <jesse.white6@gmail.com>

--- Travel/Startup.cs
+++ Travel/Startup.cs
@@ -4,6 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
+
 using Travel.Models;
 
 namespace Travel
@@ -22,6 +27,31 @@ namespace Travel
       services.AddDbContext<TravelContext>(opt =>
           opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Travel API",
+          Description = "A simple example ASP.NET Core Web API",
+          TermsOfService = new Uri("https://example.com/terms"),
+          Contact = new OpenApiContact
+          {
+            Name = "Isaac Moreno",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/isaacrmoreno"),
+            // Name = "Jesse White",
+            // Email = string.Empty,
+            // Url = new Uri("https://github.com/JesseDWhite"),
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+          }
+        });
+      });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -31,6 +61,16 @@ namespace Travel
         app.UseDeveloperExceptionPage();
       }
 
+      app.UseSwagger(c =>
+      {
+        c.SerializeAsV2 = true; // To allow backwards support capability.
+      });
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Travel API V1");
+        c.RoutePrefix = string.Empty;
+      });
       // app.UseHttpsRedirection();
 
       app.UseRouting();
